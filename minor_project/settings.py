9b6027d0219a61079c26c7584fbea453780aaa40
nikeshbhattarai1 <nikeshbhattarai39@gmail.com>
2024-03-05 09:07:38 +0545

    Vercel config done

--- minor_project/settings.py
+++ minor_project/settings.py
@@ -170,7 +170,7 @@ INTERNAL_IPS = [
 
 
 
-import os 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# import os 
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
